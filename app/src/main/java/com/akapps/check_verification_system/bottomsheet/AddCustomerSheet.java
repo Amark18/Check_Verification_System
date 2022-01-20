1e75a28ad90ae268435d83945b80114424fde631
Amar Khanshali <akhanshali2328@sdsu.edu>
2022-01-19 21:53:19 -0600

    Fixed issue of those viewed not updated in database

--- app/src/main/java/com/akapps/check_verification_system/bottomsheet/AddCustomerSheet.java
+++ app/src/main/java/com/akapps/check_verification_system/bottomsheet/AddCustomerSheet.java
@@ -132,8 +132,7 @@ public class AddCustomerSheet extends RoundedBottomSheetDialogFragment {
                             // live means up-to-date
                             Customer liveCustomer = document.toObject(Customer.class);
                             // prevents writing to database if customer was viewed today
-                            if(!customer.getDateViewed().equals(liveCustomer.getDateViewed()))
-                                firestoreDatabase.updateCustomer(liveCustomer.getCustomerUniqueId(), "dateViewed", Helper.getDatabaseDate());
+                            firestoreDatabase.updateCustomer(liveCustomer.getCustomerUniqueId(), "dateViewed", Helper.getDatabaseDate());
                             if(firestoreDatabase.updateLocalCustomer(customer, liveCustomer)) {
                                 // update customer view status
                                 // local customer data needs to be updated since there is new data
